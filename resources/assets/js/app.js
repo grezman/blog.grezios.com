6025918c30b110a006684595ce634b5ed5f43880
grezman <grezios@gmail.com>
2017-11-20 11:57:43 +0200

    The first commit

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -9,9 +9,17 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
-import Buefy from 'Buefy';
+import Buefy from 'buefy';
 
-Vue.use(Buefy);
+Vue.use(Buefy); 
+
+
+var app = new Vue({
+    el:'#app',
+    data:{}
+    
+    
+});
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
